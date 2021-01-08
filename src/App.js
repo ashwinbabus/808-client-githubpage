a1202297d2b3f5356b0e65d0e1cf5ba9e2b2af94
ashwinbabus <ashwinbabus21@gmail.com>
2021-01-08 21:54:52 +0530

    react app to github page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect } from "react";
+import { connect } from "react-redux";
+import { Route, Switch } from "react-router-dom";
+import Homepage from "./pages/homepage/homepage.component";
+import MyAccount from "./pages/my-account-page/my-account-page.component";
+import Shop from "./pages/shop/shop.component";
+import Cart from "./components/cart/cart.component";
+import CartPage from "./pages/cart-page/cart-page.component";
+import { ProtectedRoute } from "./util";
+import ThankYou from "./components/thank-you/thank-you.component";
+import Dash from "./pages/dashboard/dash.component";
+import { auth } from "./firebase/firebase";
+import { checkUserSession } from "./redux/user/user.actions";
 
-function App() {
+function App({ signInUser }) {
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        signInUser(user.uid);
+      }
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, [signInUser]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ProtectedRoute exact path="/dashboard" component={Dash} />
+      <Switch>
+        <Route path="/shop" component={Shop} />
+        <Route exact path="/" component={Homepage} />
+        <Route exact path="/my-account" component={MyAccount} />
+        <Route exact path="/cart" component={CartPage} />
+        <Route exact path="/order-success" component={ThankYou} />
+      </Switch>
+      <Cart />
     </div>
   );
 }
 
-export default App;
+const mapDispatchToProps = (dispatch) => ({
+  signInUser: (firebase_uid) => dispatch(checkUserSession(firebase_uid)),
+});
+export default connect(null, mapDispatchToProps)(App);
